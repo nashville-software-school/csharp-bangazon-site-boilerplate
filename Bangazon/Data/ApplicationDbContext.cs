79508d66b9ef7aab290810362ddb47e5de76efb4
Steve Brownlee <chortlehoort@gmail.com>
2019-06-04 11:32:48 -0500

    Update user object and more products Added Id and SecurityStamp to user object so that future migrations, if needed, do not delete and re-create the user.

--- Bangazon/Data/ApplicationDbContext.cs
+++ Bangazon/Data/ApplicationDbContext.cs
@@ -45,7 +45,8 @@ namespace Bangazon.Data {
                 .Property (b => b.DateCreated)
                 .HasDefaultValueSql ("GETDATE()");
 
-            ApplicationUser user = new ApplicationUser {
+            ApplicationUser user = new ApplicationUser
+            {
                 FirstName = "Admina",
                 LastName = "Straytor",
                 StreetAddress = "123 Infinity Way",
@@ -55,61 +56,139 @@ namespace Bangazon.Data {
                 NormalizedEmail = "ADMIN@ADMIN.COM",
                 EmailConfirmed = true,
                 LockoutEnabled = false,
-                SecurityStamp = Guid.NewGuid ().ToString ("D")
+                SecurityStamp = "7f434309-a4d9-48e9-9ebb-8803db794577",
+                Id = "00000000-ffff-ffff-ffff-ffffffffffff"
             };
-            var passwordHash = new PasswordHasher<ApplicationUser> ();
-            user.PasswordHash = passwordHash.HashPassword (user, "Admin8*");
-            modelBuilder.Entity<ApplicationUser> ().HasData (user);
+            var passwordHash = new PasswordHasher<ApplicationUser>();
+            user.PasswordHash = passwordHash.HashPassword(user, "Admin8*");
+            modelBuilder.Entity<ApplicationUser>().HasData(user);
 
             modelBuilder.Entity<PaymentType> ().HasData (
-                new PaymentType () {
+                new PaymentType()
+                {
                     PaymentTypeId = 1,
-                        UserId = user.Id,
-                        Description = "American Express",
-                        AccountNumber = "86753095551212"
+                    UserId = user.Id,
+                    Description = "American Express",
+                    AccountNumber = "86753095551212"
                 },
-                new PaymentType () {
+                new PaymentType()
+                {
                     PaymentTypeId = 2,
-                        UserId = user.Id,
-                        Description = "Discover",
-                        AccountNumber = "4102948572991"
+                    UserId = user.Id,
+                    Description = "Discover",
+                    AccountNumber = "4102948572991"
                 }
             );
 
-            modelBuilder.Entity<ProductType> ().HasData (
-                new ProductType () {
+            modelBuilder.Entity<ProductType>().HasData(
+                new ProductType()
+                {
                     ProductTypeId = 1,
-                        Label = "Sporting Goods"
+                    Label = "Sporting Goods"
                 },
-                new ProductType () {
+                new ProductType()
+                {
                     ProductTypeId = 2,
-                        Label = "Appliances"
+                    Label = "Appliances"
+                },
+                new ProductType()
+                {
+                    ProductTypeId = 3,
+                    Label = "Tools"
+                },
+                new ProductType()
+                {
+                    ProductTypeId = 4,
+                    Label = "Games"
+                },
+                new ProductType()
+                {
+                    ProductTypeId = 5,
+                    Label = "Music"
+                },
+                new ProductType()
+                {
+                    ProductTypeId = 6,
+                    Label = "Health"
+                },
+                new ProductType()
+                {
+                    ProductTypeId = 7,
+                    Label = "Outdoors"
+                },
+                new ProductType()
+                {
+                    ProductTypeId = 8,
+                    Label = "Beauty"
+                },
+                new ProductType()
+                {
+                    ProductTypeId = 9,
+                    Label = "Shoes"
+                },
+                new ProductType()
+                {
+                    ProductTypeId = 10,
+                    Label = "Automotive"
                 }
             );
 
-            modelBuilder.Entity<Product> ().HasData (
-                new Product () {
+            modelBuilder.Entity<Product>().HasData(
+                new Product()
+                {
                     ProductId = 1,
-                        ProductTypeId = 1,
-                        UserId = user.Id,
-                        Description = "It flies high",
-                        Title = "Kite",
-                        Quantity = 100,
-                        Price = 2.99
+                    ProductTypeId = 1,
+                    UserId = user.Id,
+                    Description = "It flies high",
+                    Title = "Kite",
+                    Quantity = 100,
+                    Price = 2.99
                 },
-                new Product () {
+                new Product()
+                {
                     ProductId = 2,
-                        ProductTypeId = 2,
-                        UserId = user.Id,
-                        Description = "It rolls fast",
-                        Title = "Wheelbarrow",
-                        Quantity = 5,
-                        Price = 29.99
+                    ProductTypeId = 2,
+                    UserId = user.Id,
+                    Description = "It rolls fast",
+                    Title = "Wheelbarrow",
+                    Quantity = 5,
+                    Price = 29.99
+                },
+                new Product()
+                {
+                    ProductId = 3,
+                    ProductTypeId = 3,
+                    UserId = user.Id,
+                    Description = "It cuts things",
+                    Title = "Saw",
+                    Quantity = 18,
+                    Price = 31.49
+                },
+                new Product()
+                {
+                    ProductId = 4,
+                    ProductTypeId = 3,
+                    UserId = user.Id,
+                    Description = "It puts holes in things",
+                    Title = "Electric Drill",
+                    Quantity = 12,
+                    Price = 24.89
+                },
+                new Product()
+                {
+                    ProductId = 5,
+                    ProductTypeId = 3,
+                    UserId = user.Id,
+                    Description = "It puts things together",
+                    Title = "Hammer",
+                    Quantity = 32,
+                    Price = 22.69
                 }
             );
 
             modelBuilder.Entity<Order> ().HasData (
-                new Order () {
+                new Order()
+                {
                     OrderId = 1,
                     UserId = user.Id,
                     PaymentTypeId = null
@@ -117,7 +196,8 @@ namespace Bangazon.Data {
             );
 
             modelBuilder.Entity<OrderProduct> ().HasData (
-                new OrderProduct () {
+                new OrderProduct()
+                {
                     OrderProductId = 1,
                     OrderId = 1,
                     ProductId = 1
@@ -125,13 +205,13 @@ namespace Bangazon.Data {
             );
 
             modelBuilder.Entity<OrderProduct> ().HasData (
-                new OrderProduct () {
+                new OrderProduct()
+                {
                     OrderProductId = 2,
                     OrderId = 1,
                     ProductId = 2
                 }
             );
-
         }
     }
 }
